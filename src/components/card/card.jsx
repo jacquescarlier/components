f0912d842c8038dafd86a740a54f9407370be647
jacques <lapitou60@gmail.com>
2024-01-16 14:37:25 +0100

    add key for card

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 function Card({ logements }) {
     return (
         <div className="card-style">
             {logements.map((logement) => (
-                <div className="card">
+                <div className="card" key={logement.id}>
                     <img src={logement.cover} alt={logement.title}></img>
                     <p className="card-title">{logement.title}</p>
                 </div>
